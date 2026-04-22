282bb843f07310def3d4fcfe53d29d0e3b479915
Jezvin7 <jesvinvarghese007>
2026-04-22 13:18:59 +0200

    implement student info view, app setup, and CSV visualization (Tasks 1-3)

---
+++ project1/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'project1'
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("upload/", views.upload_csv, name="upload_csv"),
+    path("reset/", views.reset_session, name="reset_session"),
+]
